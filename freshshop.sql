47d03d35df6d340b489e08aa63e907f4792e53ed
manunited-abc <19130071@st.hcmuaf.edu.vn>
2022-11-19 23:19:08 +0700

    add file sql

---
+++ freshshop.sql
@@ -0,0 +1,50 @@
+create table users(
+	id int primary key auto_increment,
+    last_name varchar(100) ,
+    first_name  varchar(100),
+    email varchar(500),
+    password varchar(500)
+);
+create table categories(
+	id int primary key auto_increment,
+    category_name varchar(100)
+);
+create table products(
+	id int primary key auto_increment,
+    product_name text,
+    desciption text,
+    prices double,
+    category_id int,
+    image text
+);
+create table reviews(
+	id int primary key auto_increment,
+    users_id int,
+    product_id int,
+    contents text,
+    starts int
+);
+create table orders(
+	id int primary key auto_increment,
+    user_id int,
+    issue_date date
+);
+create table order_detail(
+	product_id int,
+    order_id int,
+    quantity int,
+    constraint order_product_pk primary key(product_id, order_id)
+);
+create table rule_user(
+	id int primary key auto_increment,
+    user_id int,
+    rule_name varchar(100)
+);
+ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories(id);
+ALTER TABLE reviews ADD FOREIGN KEY (users_id) REFERENCES users(id);
+ALTER TABLE reviews ADD FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE orders ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE order_detail ADD FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE order_detail ADD FOREIGN KEY (order_id) REFERENCES orders(id);
+ALTER TABLE rule_user ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
