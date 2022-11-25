946cfe9761c436866ab93e331d5d300911ca0080
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2022-11-25 19:08:06 +0700

    run file

--- Swing/DigitalSignature_Tool/src/Model/AES_Algorithm.java
+++ Swing/DigitalSignature_Tool/src/Model/AES_Algorithm.java
@@ -1,34 +1,26 @@
 package Model;
 
 import java.security.*;
-import java.security.spec.*;
 import java.util.*;
 
 import javax.crypto.*;
 import javax.crypto.spec.*;
 
 public class AES_Algorithm {
-	public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+	public SecretKey generateKey(int n) throws NoSuchAlgorithmException {
 		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
 		keyGenerator.init(n);
 		SecretKey key = keyGenerator.generateKey();
 		return key;
 	}
 
-	public static SecretKey getKeyFromPassword(String password, String salt) throws Exception {
-		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-		SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
-		return secret;
-	}
-
-	public static IvParameterSpec generateIv() {
+	public IvParameterSpec generateIv() {
 		byte[] iv = new byte[16];
 		new SecureRandom().nextBytes(iv);
 		return new IvParameterSpec(iv);
 	}
 
-	public static String encrypt(String algorithm, String input, SecretKey key, IvParameterSpec iv)
+	public String encrypt(String algorithm, String input, SecretKey key, IvParameterSpec iv)
 			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
 			InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
 
@@ -38,7 +30,7 @@ public class AES_Algorithm {
 		return Base64.getEncoder().encodeToString(cipherText);
 	}
 
-	public static String decrypt(String algorithm, String cipherText, SecretKey key, IvParameterSpec iv)
+	public String decrypt(String algorithm, String cipherText, SecretKey key, IvParameterSpec iv)
 			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
 			InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
 
@@ -46,19 +38,6 @@ public class AES_Algorithm {
 		cipher.init(Cipher.DECRYPT_MODE, key, iv);
 		byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
 		return new String(plainText);
-	}
-
-	public static void main(String[] args) throws Exception {
-		AES_Algorithm aes = new AES_Algorithm();
-		String input = "baeldung";
-		SecretKey key = generateKey(128);
-		IvParameterSpec ivParameterSpec = generateIv();
-		String algorithm = "AES/CBC/PKCS5Padding";
-		String cipherText = encrypt(algorithm, input, key, ivParameterSpec);
-		String plainText = decrypt(algorithm, cipherText, key, ivParameterSpec);
-		System.out.println(cipherText);
-		System.out.println(plainText);
-
 	}
 
 }
--- Swing/DigitalSignature_Tool/src/Run.java
+++ Swing/DigitalSignature_Tool/src/Run.java
@@ -1,39 +1,51 @@
 import java.io.*;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 import Model.*;
 
 public class Run {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		// create pdf file
 		File file = new File("D:\\bill.pdf");
 		file.delete();
-		
+
 		if (file.createNewFile()) {
 			// write on file
 			String text = "Hello mấy cưng, chụy Linda nè!";
-            DAO dao = new DAO();
-            dao.writeFileTXT(text, file);
-			
-			// hash the pdf file
-		SHA512_Hashing sha = new SHA512_Hashing();
-		String hashing = sha.hashFile(file.getAbsolutePath());
-		System.out.println(hashing);
-		
-		
-		// encrypt the hashing
-		RSA_Algorithm rsa = new RSA_Algorithm(1024); //  or 2028 for size
-		try (FileOutputStream fos = new FileOutputStream("public.key")) {
-			fos.write(rsa.getPublicKey_code().getEncoded());
-		}
-		try (FileOutputStream fos = new FileOutputStream("private_key.pem")) {
-			fos.write(rsa.getPrivateKey_code().getEncoded());
-		}
-		
-		
-		System.out.println("Encrypt: " + rsa.encryptString(hashing, "private_key.pem"));
-		//System.out.println("Encrypt: " + rsa.decryptString(rsa.encryptString(hashing, "private_key.pem"), "public.key"));
+			DAO dao = new DAO();
+			dao.writeFileTXT(text, file);
 
-		
+			// hash the pdf file
+			SHA512_Hashing sha = new SHA512_Hashing();
+			String hashing = sha.hashFile(file.getAbsolutePath());
+			System.out.println(hashing);
+
+			// encrypt the hashing
+
+			// RSA
+			RSA_Algorithm rsa = new RSA_Algorithm(1024); // or 2028 for size
+			try (FileOutputStream fos = new FileOutputStream("public.key")) {
+				fos.write(rsa.getPublicKey_code().getEncoded());
+			}
+			try (FileOutputStream fos = new FileOutputStream("private_key.pem")) {
+				fos.write(rsa.getPrivateKey_code().getEncoded());
+			}
+			System.out.println("Encrypt: " + rsa.encryptString(hashing, "private_key.pem"));
+			System.out.println(
+					"Decrypt: " + rsa.decryptString(rsa.encryptString(hashing, "private_key.pem"), "public.key"));
+
+			// AES
+			AES_Algorithm aes = new AES_Algorithm();
+			SecretKey key = aes.generateKey(192); // 128/ 192/ 256
+			IvParameterSpec ivParameterSpec = aes.generateIv();
+			String algorithm = "AES/CBC/PKCS5Padding";
+			String cipherText = aes.encrypt(algorithm, hashing, key, ivParameterSpec);
+			String plainText = aes.decrypt(algorithm, cipherText, key, ivParameterSpec);
+			System.out.println("Encrypt: " + cipherText);
+			System.out.println("Decrypt: " + plainText);
+
 		} else {
 			System.out.println("File is not found!");
 		}
